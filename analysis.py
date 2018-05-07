3497fc62eb4aa94c33db5edd6bd3cd66eef1c7e3
tanya <tanyabalaraju@gmail.com>
2018-05-06 22:57:31 -0400

    different analysis works better

--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
 #load text files containing data and compare them
 from difflib import SequenceMatcher
+import re
 
 all_books = []
 for i in range(1,8):
@@ -19,12 +20,12 @@ gen_s = '"I want to know what he\'s about to do." \
     the people in the crowd cheered. They had been excited by the afterpeering theor in the...'
 
 all_books_s = ''.join(all_books)
-
-sm = SequenceMatcher(None, gen_s, all_books_s)
-print (sm.ratio())
-
-for block in sm.get_matching_blocks():
-    #print ('Generated text (%d) and HP (%d) match for % elements' %block)
-    print (block)
-    #print ('{} \n and \n {} match \n'.format(gen_s[block[0]:block[0] + block[2]], \
-            #all_books[block[1]:block[1] + block[2]]))
+all_books_sents = re.split('.,!?"()', all_books_s)
+with open('matches.txt', 'w') as f:
+    for sent in range(len(all_books_sents)):
+        sm = SequenceMatcher(None, gen_s, all_books_sents[sent])
+        for block in sm.get_matching_blocks():
+            if block[2] > 10:
+                 f.write('Match: "{}" in original \n \
+                         with "{}" in generated \n'.format(all_books_sents[sent][block[1]:block[1]+block[2]], \
+                         gen_s[block[0]:block[0]+block[2]]))
